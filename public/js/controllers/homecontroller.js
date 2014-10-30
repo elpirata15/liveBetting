8172898d9c1ba423b0a6c07b18201560e6b2b69e
elirankon <elirankon@gmail.com>
2014-10-30 23:21:29 +0200

    #1: Added login role screen in first page Added ngCookies injection ---------------------------------------------- Server: Fixed custom logger pubnub publishing

---
+++ public/js/controllers/homecontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('liveBetManager').controller('homeController', ['$scope', '$rootScope', '$cookieStore',
+function ($scope, $rootScope, $cookieStore) {
+
+    $scope.userName;
+    $scope.loginRole;
+
+    $scope.doLogin = function () {
+        if ($scope.userName && $scope.loginRole) {
+            $cookieStore.put("liveBet_user", {userName: $scope.userName, loginRole: $scope.loginRole});
+            if ($scope.loginRole == "Master")
+                $location.path("#/gameMaster");
+            else
+                $location.path("#/initGame");
+        }
+    };
+}]);
