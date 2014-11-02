ecbdfdb0faf538a5ad70caf78af790fc3ab799cf
Eliran Kononowicz <elirankon@gmail.com>
2014-11-02 17:44:30 +0200

    added session management on the way to user authorization

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(express);
 var serverLogger = require('./serverLogger');
 var gameController = require('./game');
 var bidController = require('./bid');
+
+var secretKey = "ELFcjUgNvdKyiiaXDg2mnjPUgVAx6uaVlbdcqANvqgoyZeZVIxmqlOVykkmr2hcs";
 
 var logger = new serverLogger();
 var app = express();
@@ -10,7 +15,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.session({ store: new RedisStore({
+    host: 'pub-redis-18324.us-east-1-3.3.ec2.garantiadata.com',
+    port: 18324,
+    prefix: 'sess'
+}), secret: secretKey }));
 // ##### GAME ACTIONS #####
 
 // For client - get games you can subscribe to
