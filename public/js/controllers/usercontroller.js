0e6811796a6141bf6f1eded98e45401274ee6022
elirankon <elirankon@gmail.com>
2014-11-04 21:52:29 +0200

    Added logout action Added logged in indication for testing Fixed when user reloads the page (F5), the user is logged out - Added user id and group cookies closes #25

--- public/js/controllers/usercontroller.js
+++ public/js/controllers/usercontroller.js
@@ -1,18 +1,34 @@
 angular.module('liveBetManager').controller('userController', ['$scope', '$rootScope', 'authService', '$timeout', '$location', function ($scope, $rootScope, authService, $timeout, $location) {
 
     $scope.user = {};
+    $scope.loggedIn = false;
 
     $scope.doLogin = function () {
-        if (authService.login({email:$scope.user.email, pass:$scope.user.pass})) {
-            alert('success');
-            $location.path($rootScope.returnUrl);
-        }
+        authService.login({email: $scope.user.email, pass: $scope.user.pass}, function (result) {
+            if (result) {
+                $scope.loggedIn = true;
+                if(authService.group() == "Admins" || authService.group() == "Managers" )
+                    $location.path('/gameMaster');
+                else
+                    $location.path('/eventManager');
+            }
+        });
+    };
+
+    $scope.doLogout = function () {
+        authService.logout(function (result) {
+            if (result) {
+                $scope.loggedIn = false;
+                $location.path('/');
+            }
+        });
     };
 
     $scope.doRegister = function () {
-        if (authService.register($scope.user)) {
-            $location.path('/');
-        }
+        authService.register($scope.user, function (result) {
+            if (result)
+                $scope.loggedIn = true;
+                $location.path('/');
+        });
     };
-
 }]);
--- public/js/services/authservice.js
+++ public/js/services/authservice.js
@@ -1,46 +1,45 @@
-angular.module('liveBetManager').factory('authService', ['$http', '$rootScope', function ($http, $rootScope) {
+angular.module('liveBetManager').factory('authService', ['$http', '$rootScope', '$cookieStore', function ($http, $rootScope, $cookieStore) {
     var authService = {};
 
-    var currentUser = {};
-
-    authService.login = function (creds) {
+    authService.login = function (creds, callback) {
         $http.post('/login', creds).success(function (data) {
-            currentUser = data;
-            return true;
+            $cookieStore.put("liveBetUser", data._id);
+            $cookieStore.put("liveBetGroup", data.group);
+            callback(true);
         }).error(function (data) {
             $rootScope.flash = data;
-            return false;
+            callback(false);
         });
     };
 
-    authService.logout = function () {
+    authService.logout = function (callback) {
         $http.post('/logout', null).success(function () {
-            currentUser = {};
-            return true;
+            $cookieStore.remove("liveBetUser");
+            $cookieStore.remove("liveBetGroup");
+            callback(true);
         }).error(function (data) {
             $rootScope.flash = data;
-            return false;
+            callback(false);
         });
     };
 
-    authService.register = function (user) {
+    authService.register = function (user, callback) {
         $http.post('/register', user).success(function (data) {
-            currentUser = data;
-            return true;
+            $cookieStore.put("liveBetUser", data._id);
+            $cookieStore.put("liveBetGroup", data.group);
+            callback(true);
         }).error(function (data) {
             $rootScope.flash = data;
-            return false;
+            callback(false);
         });
     };
 
     authService.group = function () {
-        if (currentUser) {
-            return currentUser.group;
-        }
+        return $cookieStore.get("liveBetGroup");
     };
 
     authService.isAuthenticated = function () {
-        return currentUser != {};
+        return $cookieStore.get("liveBetUser");
     };
 
     return authService;
