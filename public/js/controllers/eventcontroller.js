b3712a258e69ca2d990b58fa595aa0cc771bb372
Ariel Fisher <fishondor@gmail.com>
2014-11-20 03:01:46 +0200

    event page and corner template
    example for activating the corner template while preparing an array that
    contains the bet details with a flow of enabling buttons.

--- public/js/controllers/eventcontroller.js
+++ public/js/controllers/eventcontroller.js
@@ -2,11 +2,101 @@ angular.module('liveBetManager').controller('eventController',['$scope','$rootSc
     function($scope, $rootScope, PubNub, betManagerService,$timeout, localStorageService,ModalService) {
         $scope.connected = false;
         $scope.log = [];
-        $scope.game = localStorageService.get('game');
-        $scope.betSyntax = [];
+		console.log("herh ehrer");
+    //    $scope.game = localStorageService.get('game');//this contains following array
+		//comes from gave.events
+		$scope.events = [{'eventName':'Foul','eventTemplate':'foul.html'},
+						{'eventName':'Free Kick','eventTemplate':'Free_Kick.html'},
+						{'eventName':'Corner','eventTemplate':'Corner.html'},
+						{'eventName':'Out','eventTemplate':'Out.html'},
+						{'eventName':'Offside','eventTemplate':'Offside.html'},
+						{'eventName':'Penalty','eventTemplate':'Penalty.html'},
+						{'eventName':'Substitution','eventTemplate':'Substitution.html'}];
+		//comes from game.teams
+		$scope.teams = [{'teamName':"macabi tel-aviv", 'players':[
+							{'playerName':"Messi"},
+							{'playerName':"Ronaldo"},
+							{'playerName':"Neymar"},
+							{'playerName':"Haim"},
+							{'playerName':"yafyoofi"},
+							{'playerName':"mooshon"},
+							{'playerName':"eliran"},
+							{'playerName':"adiel"},
+							{'playerName':"uziel"},
+							{'playerName':"yerachmiel"},
+							{'playerName':"tzidki"}],
+							'active':false},
+							{'teamName':"beytar jerusalem", 'players':[
+							{'playerName':"Messi1"},
+							{'playerName':"Ronaldo1"},
+							{'playerName':"Neymar1"},
+							{'playerName':"Haim1"},
+							{'playerName':"yafyoofi1"},
+							{'playerName':"mooshon1"},
+							{'playerName':"eliran1"},
+							{'playerName':"adiel1"},
+							{'playerName':"uziel1"},
+							{'playerName':"yerachmiel1"},
+							{'playerName':"tzidki1"}],
+							'active':false}
+						];
+		/*
+		$scope.theteam = '?';
+		$scope.setTeam = function(teamName) {
+			console.log('in teams'+teamName);
+			$scope.theteam = teamName;
+			for (var i=0; i<$scope.teams.length; I++){
+				$scope.theteam[i]
+			}
+			$scope.kicker = '?';
+		};
+		
+		$scope.kicker = '?';
+		$scope.setPlayer = function(playerName) {
+			console.log('in players'+playerName);
+			$scope.kicker = playerName;
+		};*/
+		$scope.disableOpen = true;
+		$scope.disableClose = true;
+		$scope.disableOutcomes = true;
+		
+		$scope.betArray = {'betDescription':'','betOptions':[]};
+		$scope.disableTheRest = function(){
+			for (var i=0; i<$scope.teams.length; i++){
+				$scope.teams[i].active = false;
+			}
+			$scope.betArray.betDescription = null;
+		}
+		$scope.allowBet = function(){
+			console.log('say is=='+$scope.betArray.betDescription);
+			console.log('say is=='+$scope.betArray.betOptions);
+			$scope.disableOpen = false;
+		}
+		$scope.openBet = function(){
+			//do somthing here to send betarray to the server
+			$scope.disableClose = false;
+			$scope.disableOpen = true;
+		}
+		$scope.openSubmitOutcomes = function(){
+			$scope.disableOpen = true;
+			$scope.disableClose = true;
+			$scope.disableOutcomes = false;
+		}
+		
+		$scope.templateroot = 'partials/eventstemplates/';				
+		$scope.currentEvent = $scope.templateroot+'Choose_Event.html';
+		
+		$scope.changeEventTemplate = function (template){
+			console.log("the template==="+template);
+			$scope.currentEvent = $scope.templateroot+template;
+		}
+		
+		
+	
+    /*    $scope.betSyntax = [];
         $scope.teamRosters = {};
-        $scope.selectedTeamIndex = 0;
-        PubNub.ngSubscribe({
+        $scope.selectedTeamIndex = 0;*/
+    /*    PubNub.ngSubscribe({
             channel : $scope.game._id,
             message : function(message){
 
@@ -20,9 +110,9 @@ angular.module('liveBetManager').controller('eventController',['$scope','$rootSc
             disconnect: function(data){
                 $scope.$apply(function(){$scope.connected = false;});
             }
-        });
+        });*/
 
-        $scope.setSyntax = function($event, index){
+    /*    $scope.setSyntax = function($event, index){
           $scope.betSyntax[index] = $event.currentTarget.innerHTML;
         };
 
@@ -50,5 +140,5 @@ angular.module('liveBetManager').controller('eventController',['$scope','$rootSc
                 });
             });
 
-        };
+        };*/
     }]);
