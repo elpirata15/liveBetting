229f141cddd9c379fe03ac00d393de98333746a2
elirankon <elirankon@gmail.com>
2014-11-27 22:07:08 +0200

    included batch files to run server locally using test/prod environment variables

---
+++ runLocalProd.bat
@@ -0,0 +1,6 @@
+@echo off
+set MONGOLAB_URI=mongodb://ds055980.mongolab.com:55980/heroku_app32018241
+set PUBNUB_PUBLISH_KEY=pub-c-0dd5cf68-51d4-46f1-afeb-e8eeb4780703
+set PUBNUB_SUBSCRIBE_KEY=sub-c-8bec0072-7663-11e4-af64-02ee2ddab7fe
+@echo on
+node index.js
---
+++ runLocalTest.bat
@@ -0,0 +1,6 @@
+@echo off
+set MONGOLAB_URI=mongodb://ds055980.mongolab.com:55980/heroku_app32018520
+set PUBNUB_PUBLISH_KEY=pub-c-d2e656c9-a59e-48e2-b5c5-3c16fe2124d2
+set PUBNUB_SUBSCRIBE_KEY=sub-c-71b821d4-7665-11e4-af64-02ee2ddab7fe
+@echo on
+node index.js
