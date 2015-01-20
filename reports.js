88379cf426808b7e34cddd36f76caabb03d37d09
elirankon <elirankon@gmail.com>
2015-01-20 22:54:29 +0200

    Improved reporting mapreduce

--- reports.js
+++ reports.js
@@ -9,23 +9,33 @@ exports.getRevenueReport = function(req, res){
 
             for(var i in this.bids){
                 for(var j in this.bids[i].bidOptions){
-                    emit(this.bids[i].id,{totalPoolAmount: this.bids[i].totalPoolAmount, participants:this.bids[i].bidOptions[j].participants.length});
+                    emit(this.bids[i].id,{totalPoolAmount: this.bids[i].totalPoolAmount, participants:this.bids[i].bidOptions[j].participants.length, gameName: this.gameName, gameId: this._id});
                 }
             }
         },
         reduce: function(k, vals){
-            var result = {poolAmount: 0, participants:0};
+            var result = {poolAmount: 0, participants:0, gameName: null, gameId: null};
             for(var i in vals){
                 result.poolAmount = vals[i].totalPoolAmount;
                 result.participants += vals[i].participants;
+                result.gameName = vals[i].gameName;
+                result.gameId = vals[i].gameId;
             }
             return result;
-        },
-        query: {_id: req.params.id}
+        }
+        //,query: {_id: req.params.id}
     };
 
     dbOperations.GameModel.mapReduce(o, function(err, result){
-       res.status(200).send(result);
+        var finalResult = {};
+        for(var i in result){
+            if(!finalResult[result[i].value.gameId]){
+                finalResult[result[i].value.gameId] = {totalPools: 0, totalParticipants: 0,gameName: result[i].value.gameName};
+            }
+            finalResult[result[i].value.gameId].totalPools += result[i].value.poolAmount;
+            finalResult[result[i].value.gameId].totalParticipants += result[i].value.participants;
+        }
+        res.status(200).send(finalResult);
     });
 
 };
