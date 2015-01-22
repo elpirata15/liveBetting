c069951d7325e225092bff19a4dc3b2a38016824
Eliran Kononowicz <elirankon@gmail.com>
2015-01-22 06:51:00 +0000

    fixed reporting navigation

--- public/js/controllers/reporting/reportingcontroller.js
+++ public/js/controllers/reporting/reportingcontroller.js
@@ -1,5 +1,6 @@
 
 angular.module('liveBetManager').controller('reportingController', ['$scope', '$rootScope', '$route', '$routeParams', 'PubNub', 'betManagerService', 'teamsService', 'ModalService', '$timeout', 'dialogs', function ($scope, $rootScope, $route,$routeParams, PubNub, betManagerService, teamsService, ModalService, $timeout,dialogs) {
+    $scope.currentView = location.hash.slice(2);
     $scope.changeView = function(view){
       location.href=view;
     };
